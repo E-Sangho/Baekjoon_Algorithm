be0af8197c076e1ba6b0cbf74589aacdf81298ff
Sangho <istrangeho@gmail.com>
2021-06-17 01:05:44 +0900

    2021-06-17

--- 11651.cpp
+++ 11651.cpp
@@ -2,35 +2,43 @@
 #include <algorithm>
 #include <vector>
 
-class Point
-{
-public:
+class Point{
+private:
     int x;
     int y;
-
-    Point(const int &x_input, const int &y_input)
-    {
+public:
+    Point(const int &x_input, const int &y_input){
         x = x_input;
         y = y_input;
+    }
+
+    const int getx(){
+        int a = x;
+        return a;
+    }
+
+    const int gety(){
+        int b = y;
+        return b;
     }
 };
 
 bool compare(Point &a, Point &b){
-    if(a.x == b.x) return a.y < b.y;
-    else  return a.x < b.x;
+    if(a.gety() == b.gety()) return a.getx() < b.getx();
+    else return a.gety() < b.gety();
 }
 
 int main(){
     int N;
     scanf("%d", &N);
+    int x = 0, y = 0;
     std::vector<Point> p;
-    int x, y;
     for(int i = 0; i < N; ++i){
         scanf("%d %d", &x, &y);
         p.push_back(Point(x,y));
     }
     sort(p.begin(), p.end(), compare);
     for(int i = 0; i < N; ++i){
-        printf("%d %d\n", p[i].x, p[i].y);
+        printf("%d %d\n", p[i].getx(), p[i].gety());
     }
 }
