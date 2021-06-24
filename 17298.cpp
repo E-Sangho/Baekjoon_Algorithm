ec1094782b14965d992972fd1d9fb721a1bc0038
Sangho <istrangeho@gmail.com>
2021-06-24 15:30:23 +0900

    2021-06-24

--- 17298.cpp
+++ 17298.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <vector>
-
 using namespace std;
-
-void NGE(vector<int> v)
-{
-    int n = v.size();
-    for(int i = 0; i < n; ++i)
-    {
-        int cnt = 0, ans = -1;
-        for(int j = n - 1; j > i; --j)
-        {
-            if(v[i] < v[j]) ans = v[j];
-        }
-        cout << ans << ' ';
-    }
-}
 
 int main()
 {
@@ -24,13 +9,35 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, temp = 0;
+    int N, temp = 0, index = 0;
     cin >> N;
-    vector<int> v;
+    stack<int> s;
+    vector<int> v, ans(N, -1);
     for(int i = 0; i < N; ++i)
     {
         cin >> temp;
         v.push_back(temp);
     }
-    NGE(v);
+    
+    s.push(0);
+    for(int i = 1; i < N; ++i)
+    {
+        while(!s.empty() && v[s.top()] < v[i])
+        {
+            ans[s.top()] = v[i];
+            s.pop();
+
+        }
+        s.push(i);
+    }
+
+    for(int i = 0; i < N; ++i)
+    {
+        cout << ans[i] << ' '; 
+    }  
 }
+/* 
+ *  stack s = {s1, s2, ... , s.top} s.t. v[s1] >= v[s2] >= ... >= v[s.top]
+ *  1. v[i] <= v[s.top]  --> s = {s1, s2, ... , s.top, s[i]} s.t. v[s1] >= v[s2] >= ... >= v[s.top] >= v[input]
+ *  2. v[i] >  v[s.top]  --> while(!s.empty() && v[s.top] < v[i]) ans[s.top] = v[i], s.pop()
+ */ 
