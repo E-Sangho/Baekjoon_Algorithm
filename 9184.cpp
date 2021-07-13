8e0f222ddd7c13aeb206961d5ce30c1ab7ca440f
Sangho <istrangeho@gmail.com>
2021-07-13 13:56:17 +0900

    modify 9184

--- 9184.cpp
+++ 9184.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-vector<vector<vector<int>>> v(101, vector<vector<int>>(101, vector<int>(101,0)));
+const int maxn = 21;
+
+int v[maxn][maxn][maxn];
 
 int w(int a, int b, int c)
 {
-    if(v[a + 50][b + 50][c + 50] != 0) return v[a + 50][b + 50][c + 50];
-    else if(a <= 0 || b <= 0 || c <= 0)
-    {
-        v[a + 50][b + 50][c + 50] = 1;
-        return 1;
+    if(a <= 0 || b <= 0 || c <= 0) return 1;
+    
+    else if(a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+    
+    if(!v[a][b][c]){
+        if(a < b && b < c){
+            v[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+        }
+        else{
+            v[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+        }
     }
-    else if(a > 20 || b > 20 || c > 20)
-    {
-        v[a + 50][b + 50][c + 50] = w(20, 20, 20);
-        return v[a + 50][b + 50][c + 50];
-    }
-    else if(a < b && b < c)
-    {
-        v[a + 50][b + 50][c + 50] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
-        return v[a + 50][b + 50][c + 50];
-    }
-    else
-    {
-        v[a + 50][b + 50][c + 50] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
-        return v[a + 50][b + 50][c + 50];
-    }
+    
+    return v[a][b][c];
 }
 
 int main()
