b7618f87198ad976c9d6bd5e983672d3426dee89
Sangho <istrangeho@gmail.com>
2021-07-14 12:38:39 +0900

    1904.cpp modify

--- 1904.cpp
+++ 1904.cpp
@@ -4,16 +4,6 @@ using namespace std;
 
 int tile[1000001];
 
-int cal(const int &n)
-{
-    if(n == 1) return 1;
-    else if(n == 2) return 2;
-    else if(tile[n] == 0){
-        tile[n] = (cal(n-1)+ cal(n-2)) % 15746;
-    }
-    return tile[n];
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -22,16 +12,13 @@ int main()
 
     int N;
     cin >> N;
-    cout << cal(N);
+
+    tile[1] = 1;
+    tile[2] = 2;
+    for(int i = 3; i <= N; ++i){
+        tile[i] = (tile[i-1] + tile[i-2]) % 15746;
+    }
+    cout << tile[N];
 
     return 0;
 }
-
-
-/* N = 1    1  
- * N = 2    2    
- * N = 3    3
- * N = 4    5
- * N = 5    
- * 
- */
