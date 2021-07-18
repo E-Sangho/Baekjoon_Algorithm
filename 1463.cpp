1a96795e4853e9fbbd2d7471fd71266e5c01f238
Sangho <istrangeho@gmail.com>
2021-07-18 10:39:46 +0900

    2021--7-18

--- 1463.cpp
+++ 1463.cpp
@@ -3,14 +3,10 @@
 
 using namespace std;
 
-int dp[1000001];
-
 int goUp(int n){
     if(n <= 1) return 0;
-    if(dp[n] != 0) return dp[n];
     else{
-        dp[n] = min(n % 3 + goUp(n/3) + 1, n % 2 + goUp(n/2) + 1);
-        return dp[n];
+        return min(n % 3 + goUp(n/3) + 1, n % 2 + goUp(n/2) + 1);
     }
 }
 
