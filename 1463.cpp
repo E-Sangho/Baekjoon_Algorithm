1c3ce332e4054d6a99e70e070910a92920c52ce6
Sangho <istrangeho@gmail.com>
2021-07-18 10:38:51 +0900

    2021-07-18

--- 1463.cpp
+++ 1463.cpp
@@ -5,38 +5,22 @@ using namespace std;
 
 int dp[1000001];
 
+int goUp(int n){
+    if(n <= 1) return 0;
+    if(dp[n] != 0) return dp[n];
+    else{
+        dp[n] = min(n % 3 + goUp(n/3) + 1, n % 2 + goUp(n/2) + 1);
+        return dp[n];
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, temp;
+    int N;
     cin >> N;
-    dp[1] = 0;
-    for(int i = 2; i <=N; ++i){
-        if(i % 3 ==0) temp = dp[i/3];
-        else if(i % 2 ==0) temp = dp[i/2];
-        else{
-            temp = dp[i-1]; 
-        }
-        dp[i] = temp + 1;
-    }
-    cout << dp[N];
+    cout << goUp(N);
     return 0;
 }
-/*
- *  1 = 1
- *  2 = 2
- *  3 = 2
- *  4 = 3
- *  5 = 4
- *  6 = 3
- *  7 = 4
- *  8 = 4
- *  9 = 3
- * 
- * case #1) N is divided by 3   dp[N/3] + 1
- * case #2) N is divided by 2   dp[N/2] + 1
- * case #3) N is non-dividable by 2 or 3    dp[N-1] + 1
- *
- */
