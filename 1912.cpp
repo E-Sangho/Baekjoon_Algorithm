a7e56c4755033716968b81f3d188c4fcf72dc4b9
Sangho <istrangeho@gmail.com>
2021-07-21 15:52:11 +0900

    2021-07-21

---
+++ 1912.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    int input[n], dp[n];
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> input[i];
+    }
+    dp[0] = input[0];
+    int global_max = dp[0];
+    for(int i = 1; i < n; ++i)
+    {
+        dp[i] = max(input[i], dp[i-1] + input[i]);
+        global_max = max(global_max, dp[i]);
+    }
+    
+    cout << global_max;
+    return 0;
+}
